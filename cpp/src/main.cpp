5adea7378ceb4432fba24e3447f4b9d97cb633e6
Tommy Yardley <tommyyards@msn.com>
2022-08-13 17:57:57 +0100

    update example to include boost

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,5 +1,9 @@
+#include <boost/format.hpp>
+#include <chrono>
+#include <ctime>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  auto current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::cout << boost::format("Hello World!\nThe time is: %s") % ctime(&current_time) << std::flush;
 }
